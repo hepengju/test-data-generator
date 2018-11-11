94f180a760d933bb933fb4890262460bb229c955
hepengju <he_pengju@163.com>
2018-11-11 17:19:49 +0800

    去掉SpringBoot测试

--- src/test/java/com/hepengju/data/TestDataGeneratorApplicationTests.java
+++ src/test/java/com/hepengju/data/TestDataGeneratorApplicationTests.java
@@ -1,15 +1,10 @@
 package com.hepengju.data;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class TestDataGeneratorApplicationTests {
 
-	@Test
+	//@Test
 	public void contextLoads() {
 	}
 
